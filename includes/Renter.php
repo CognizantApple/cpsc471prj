0b1d12ec0857329d080ddd99efda3b1a3c474d80
Elaurora <joshua.walters@ucalgary.ca>
2016-11-10 16:37:23 -0700

    ALMOST (but proly not)finished submit_home_form function

--- includes/Renter.php
+++ includes/Renter.php
@@ -1,12 +1,22 @@
 <?php
 module_load_include("php", "cpsc471prj", "includes/DBConf");
+module_load_include("php", "cpsc471prj", "includes/DBTableInstance");
 
-class Renter {
+class Renter extends DBTableInstance{
+	
+	protected $id_p;
+	protected $account_uid_p;
+	protected $name_p;
+	protected $is_adult_p;
+	protected $birth_time_p;
 	
 	/**
 	 * Does nothing
 	 */
-	public function Renter(){ }
+	public function Renter($type = 'new', $data = array()){
+		$this->tableName = DBConf::$renter;
+		parent::__construct($type, $data);
+	}
 	
 	public function AddNewRenter($NewRenterInfo){
 		$SQLQuery = "INSERT INTO ".DBConf::$renter . ' (';
