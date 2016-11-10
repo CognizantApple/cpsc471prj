c674d2c7fc55f743ec2645369b27ddef21b60687
Andy <andyschndr@gmail.com>
2016-11-10 16:02:47 -0700

    added date thingy, start of season.php class and adding seasons code in module file

---
+++ includes/Season.php
@@ -0,0 +1,23 @@
+<?php
+
+module_load_include("php", "cpsc471prj","includes/DBconf");
+/**
+ * Class for making and adding seasons
+ * @author andys
+ *
+ */
+class Season{
+	
+	/**
+	 * empty constructor
+	 */
+	public function Season(){}
+	
+	/**
+	 * adds the stuff in $values into da table
+	 * @param unknown $values
+	 */
+	public function addSeason($values){
+		
+	}
+}
