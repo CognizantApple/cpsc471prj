4d0a8c9c582eb36264f6724ed98b8e0631249fcc
Elaurora <joshua.walters@ucalgary.ca>
2016-11-10 15:20:56 -0700

    Added a bunch of new functionality for the home_form_submit

---
+++ includes/Renter.php
@@ -0,0 +1,26 @@
+<?php
+
+class Renter {
+	
+	/**
+	 * Does nothing
+	 */
+	public function Renter(){
+		
+	}
+	
+	public function AddNewRenter($NewUSerInfo){
+		$SQLQuery = "INSERT INTO ";
+		db_query($SQLQuery);
+	}
+	
+	
+	
+}
+
+
+
+
+
+
+
--- includes/User.php
+++ includes/User.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * Class for checking user access
+ * Corresponds to the Rental_Account
  * @author Patrick
  *
  */
@@ -10,6 +11,14 @@ class User {
 	 * Does nothing
 	 */
 	public function User() {}
+	
+	/**
+	 * Adds the new data to the user. Assumes $NewUserData has the proper variables
+	 * Assumes that the information has been checked for validity
+	 */
+	public function AddNewUser($NewUserData){
+		
+	}
 	
 	/**
 	 * Get the current user's UID
@@ -71,5 +80,22 @@ class User {
 	 */
 	public function hasPermission($permissionName) {
 		return user_access($permissionName);
+	}
+	
+	public static function isCreditCardValid($numberInQuestion){
+		return is_numeric($numberInQuestion);
+	}
+	
+	public static function isPhoneNumberValid($numberInQuestion){
+		$numberInQuestion = str_replace(array('-', '(', ')', ' '), '', $numberInQuestion);
+		if(is_numeric($numberInQuestion)){
+			
+			$lengthOfString = strlen($numberInQuestion);
+			if($lengthOfString == 9 || $lengthOfString == 10){
+				
+				return true;
+			}
+		}
+		return false;
 	}
 }
