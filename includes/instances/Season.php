fdca414f7368b3b413ab7a315a059a1abba7e508
Andy <andyschndr@gmail.com>
2016-11-20 16:03:29 -0700

    added temporary modifications to season, and filled out manager_view_seasons_form. trying to get manager_create_seasons_from back.

--- includes/instances/Season.php
+++ includes/instances/Season.php
@@ -1,7 +1,7 @@
 <?php
 
-module_load_include("php", "cpsc471prj","includes/DBconf");
-module_load_include("php", "cpsc471prj","includes/DBTableInstance");
+module_load_include("php", "cpsc471prj", "includes/database/DBConf");
+module_load_include("php", "cpsc471prj", "includes/database/DBTableInstance");
 /**
  * Class for making and adding seasons
  * @author andys
@@ -9,7 +9,24 @@ module_load_include("php", "cpsc471prj","includes/DBTableInstance");
  */
 class Season extends DBTableInstance {
 	
-	protected $id_p;
+	/**
+	 * The start time of the season.
+	 * serves as primary key.
+	 * @var int
+	 */
+	protected $start_time;
+	
+	/**
+	 * The end time of the season.
+	 * @var int
+	 */
+	protected $end_time;
+	
+	/**
+	 * The name of the season
+	 * @var varchar(60)
+	 */
+	protected $name;
 	
 	/**
 	 * Construct a renter by passing it some values
